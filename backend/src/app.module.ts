0d6c7695701f372e418f788464849303a76d36c7
Diego Dimitroff Petcoff <82569859+DiegoDimitroffPetcoff@users.noreply.github.com>
2023-02-13 15:56:42 +0000

    Creating User

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserModule } from './user/user.module';
+import { UsersModule } from './users/users.module';
+import { TypeOrmModule  } from '@nestjs/typeorm'
 
 @Module({
-  imports: [UserModule],
+  imports: [TypeOrmModule.forRoot({
+    type:'mysql',
+    host:'localhost',
+    username:'root',
+    password:'pass',
+    database:'dbs',
+    entities:[__dirname + '/**/*.entity{.ts,.js}']
+  }),
+    UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
