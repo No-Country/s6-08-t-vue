c026e8d8f7adb8cb6d22f84cf5f26abdd486a425
Diego Dimitroff Petcoff <82569859+DiegoDimitroffPetcoff@users.noreply.github.com>
2023-02-14 11:33:13 +0000

    Adding config to connect on the DBS

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,20 +2,34 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
-import { TypeOrmModule  } from '@nestjs/typeorm'
+import { TypeOrmModule } from '@nestjs/typeorm'
 
+
+
+//AQUIE AGREGUE LA INFO COLOCACADA POR YOHAN
 @Module({
-  imports: [TypeOrmModule.forRoot({
-    type:'mysql',
-    host:'localhost',
-    username:'root',
-    password:'pass',
-    database:'dbs',
-    entities:[__dirname + '/**/*.entity{.ts,.js}'],
-    synchronize:true
-  }),
-    UsersModule],
+  imports: [
+    ConfigModule.forRoot({
+      load: [EnvConfiguration],
+      validationSchema: JoiValidationSchema,
+    }), UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor() {
+    console.log(process.env);
+  }
+}
+
+
+
+//imports: [TypeOrmModule.forRoot({
+//  type:'mysql',
+//  host:'localhost',
+//  username:'root',
+//  password:'pass',
+//  database:'dbs',
+//  entities:[__dirname + '/**/*.entity{.ts,.js}'],
+//  synchronize:true
+//})  
--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -1,7 +1,21 @@
 import { Injectable } from '@nestjs/common';
 
+//AGREGO EL ConfigService SEGUN RAMA DE YOAHN
+import { ConfigService } from '@nestjs/config';
+
+
 @Injectable()
 export class AppService {
+
+  private defaultLimit: number;
+  constructor(private readonly configService: ConfigService) {
+    this.defaultLimit = configService.get<number>('defaultLimit');
+    console.log(this.defaultLimit, 'prueba');
+  }
+
+
+
+
   getHello(): string {
     return 'Hello World!';
   }
