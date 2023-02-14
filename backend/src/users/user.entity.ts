a5a19da1290c5c46d418e2a689828aa6b0fbc42b
Diego Dimitroff Petcoff <82569859+DiegoDimitroffPetcoff@users.noreply.github.com>
2023-02-14 09:43:15 +0000

    Entity User Created

--- backend/src/users/user.entity.ts
+++ backend/src/users/user.entity.ts
@@ -1,7 +1,31 @@
-class User{
-    id
-    username
-    password
-    createdAt
-    authStrategy
+import { Entity, Column, PrimaryGenerateColum } from 'typeorm'
+
+
+//@Entity() class User... Lo que va a hacer es pluralizar el nombre
+// de manera tal que si quiero cambiarlo puedo utilizar la sig sintax
+//
+//@Entity({name: 'Usuarios'})
+//class User{
+//
+//
+
+
+@Entity({ name: 'Users' })
+export class User {
+
+    //PrimaryGenerateColum Permite generar el ID por si solo
+    @PrimaryGenerateColum()
+    id: number
+
+    //unique va a obligar a que solo exista un nombre igual
+    @Column({ unique: true })
+    username: string
+    @Column()
+    password: string
+    //defaul ya viene con MySql y 'CURRENT_TIMESTAMP' refiere a colocar la fecha actual
+    @Column({ type: 'datetime', default: () => 'CURRENT_TIMESTAMP' })
+    createdAt: Date
+    //nullable funciona para indicar que no es requerido
+    @Column({ nullable: true })
+    authStrategy: string
 }
