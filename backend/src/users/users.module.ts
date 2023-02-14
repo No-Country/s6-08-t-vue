52a70a8f082cda2fc6648e7c71cb38d51a255e86
Diego Dimitroff Petcoff <82569859+DiegoDimitroffPetcoff@users.noreply.github.com>
2023-02-14 11:08:51 +0000

    Add orm

--- backend/src/users/users.module.ts
+++ backend/src/users/users.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { User } from './user.entity'
 
 @Module({
+  import: [TypeOrmModul.forFeature({User})],
   controllers: [UsersController],
   providers: [UsersService]
 })
--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -1,4 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { User } from './user.entity';
+import { InjectRepository } from '@nestjs/typeorm'
+import { Repository } from 'typeorm'
+//puede ser pensado como una biblioteca que se comunica
+//con la dbs para hacer consultas
+import { CreateUserDto } from './dto/create-user.dto'
 
+
+//Lo primero que tengo que hacer es traer la entidad
 @Injectable()
-export class UsersService {}
+export class UsersService {
+
+    //creo el constructor -Injecto un repositorio
+    //le doy un nombre (userRepository) 
+    //y le digo que tipo de dato es (Respository) el cual importo
+    //desde 'typeorm'
+    //private: para que se instancie automaticamente
+    constructor(@InjectRepository(User) private userRepository: Respository<User>){}
+
+    createUser(user: CreateUserDto){
+        const newUser = this.userRepository.create(user)
+        return this.userRepository.save(newUser)
+    }
+
+
+}
