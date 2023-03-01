1111b69ca0b75869faefffb92f4a346f4f8bcb43
Yenifer Ramirez <yeniferramirez11@gmail.com>
2023-03-01 07:42:46 -0400

    deshacer cambio index de routes

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,5 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/CFHomeView.vue";
-import AboutView from "../views/CFAboutView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -16,7 +15,7 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: AboutView,
+      component: () => import("../views/CFAboutView.vue"),
     },
     {
       path: "/detail",
@@ -54,15 +53,10 @@ const router = createRouter({
       component: () => import("../views/CFProfileView.vue"),
     },
     {
-      path: "/history",
-      name: "history",
-      component: () => import("../views/CFHistoryView.vue"),
-    },
-    /*{
       path: "/:pathMatch(.*)*",
       name: "PageNotFound",
       component: () => import("../views/CFNotFound.vue"),
-    },*/
+    },
   ],
 });
 
